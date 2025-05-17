f0fe5642c60bf4b5d5ae3491c98470ad7f2dd0da
Marco Lee <ylee0147@student.monash.edu>
2025-05-18 05:15:08 +1000

    successfully used get account and get champion masteries requests

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,6 @@ import index from "./index.html";
 
 const server = serve({
   routes: {
-    // Serve index.html for all unmatched routes.
-    "/*": index,
-
     "/api/hello": {
       async GET(req) {
         return Response.json({
@@ -27,6 +24,63 @@ const server = serve({
         message: `Hello, ${name}!`,
       });
     },
+
+    "/api/riot/account/:gameName/:tagLine": async req => {
+      const { gameName, tagLine } = req.params;
+      const RIOT_API_KEY = process.env.RIOT_API_KEY;
+      const url = `https://asia.api.riotgames.com/riot/account/v1/accounts/by-riot-id/${encodeURIComponent(gameName)}/${encodeURIComponent(tagLine)}`;
+
+      // @ts-ignore
+      const riotRes = await fetch(url, {
+        headers: {
+          "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36",
+          "Accept-Language": "en-US,en;q=0.9,zh-TW;q=0.8,zh-CN;q=0.7,zh;q=0.6",
+          "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
+          "Origin": "https://developer.riotgames.com",
+          "X-Riot-Token": RIOT_API_KEY,
+        },
+      });
+
+      if (!riotRes.ok) {
+        return new Response(JSON.stringify({ error: "Failed to fetch Riot account" }), {
+          status: riotRes.status,
+          headers: { "Content-Type": "application/json" },
+        });
+      }
+
+      const data = await riotRes.json();
+      return Response.json(data);
+    },
+
+    "/api/lol/champion-masteries/:puuid": async req => {
+      const { puuid } = req.params;
+      const RIOT_API_KEY = process.env.RIOT_API_KEY;
+      const url = `https://oc1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-puuid/${encodeURIComponent(puuid)}`;
+
+      // @ts-ignore
+      const riotRes = await fetch(url, {
+        headers: {
+          "X-Riot-Token": RIOT_API_KEY,
+          "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36",
+          "Accept-Language": "en-US,en;q=0.9,zh-TW;q=0.8,zh-CN;q=0.7,zh;q=0.6",
+          "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
+          "Origin": "https://developer.riotgames.com"
+        },
+      });
+
+      if (!riotRes.ok) {
+        return new Response(JSON.stringify({ error: "Failed to fetch Riot account" }), {
+          status: riotRes.status,
+          headers: { "Content-Type": "application/json" },
+        });
+      }
+
+      const data = await riotRes.json();
+      return Response.json(data);
+    },
+
+    // Serve index.html for all unmatched routes.
+    "/*": index,
   },
 
   development: process.env.NODE_ENV !== "production" && {
